da3c29f6b41a3f65275949228a4842dd28e728f2
Cameron Dimitroff <hawtrods@gmail.com>
2018-04-28 19:35:43 -0700

    supp

--- langy.js
+++ langy.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
 
-var stuff = require("./script.js");
-var inquirer = require("inquirer");
+// const stuff = require("./app.js");
+const inquirer = require("inquirer");
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+const params = { screen_name: "keeelacam" };
+client.get("statuses/user_timeline", params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
 
-spotify.?
+client.get("search/tweets", { q: "node.js" }, function(
+  error,
+  tweets,
+  response
+) {
+  console.log(tweets);
+});
